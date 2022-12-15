bfc4c3db37bf02459d385b8789d4301886eced61
Camila Heredia <cs.herediavalverde@gmail.com>
2022-12-14 21:44:19 -0300

    :sparkles: Feat: Add more information in the table of Stats Page

--- js/stats.js
+++ js/stats.js
@@ -1,187 +1,185 @@
-const objectEventsStats = {
-    "fechaActual": "2022-01-01",
-    "eventos": [
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Feriadecomidas7.jpg",
-    "name": "Collectivities Party",
-    "date": "2021-12-12",
-    "description": "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-    "category": "Food Fair",
-    "place": "Room A",
-    "capacity": 45000,
-    "assistance": 42756,
-    "price": 5
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Feriadecomidas2.jpg",
-    "name": "Korean style",
-    "date": "2021-08-12",
-    "description": "Enjoy the best Korean dishes, with international chefs and awesome events.",
-    "category": "Food Fair",
-    "place": "Room A",
-    "capacity": 45000,
-    "assistance": 42756,
-    "price": 10
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Salidaalmuseo5.jpg",
-    "name": "Jurassic Park",
-    "date": "2021-11-02",
-    "description": "Let's go meet the biggest dinosaurs in the paleontology museum.",
-    "category": "Museum",
-    "place": "Field",
-    "capacity": 82000,
-    "assistance": 65892,
-    "price": 15
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Salidaalmuseo1.jpg",
-    "name": "Parisian Museum",
-    "date": "2022-11-02",
-    "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-    "category": "Museum",
-    "place": "Paris",
-    "capacity": 8200,
-    "estimate": 8200,
-    "price": 3500
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Fiestadedisfraces2.jpg",
-    "name": "Comicon",
-    "date": "2021-02-12",
-    "description": "For comic lovers, all your favourite characters gathered in one place.",
-    "category": "Costume Party",
-    "place": "Room C",
-    "capacity": 120000,
-    "assistance": 110000,
-    "price": 54
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Fiestadedisfraces1.jpg",
-    "name": "Halloween Night",
-    "date": "2022-02-12",
-    "description": "Come with your scariest costume and win incredible prizes.",
-    "category": "Costume Party",
-    "place": "Room C",
-    "capacity": 12000,
-    "estimate": 9000,
-    "price": 12
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Conciertodemusica1.jpg",
-    "name": "Metallica in concert",
-    "date": "2022-01-22",
-    "description": "The only concert of the most emblematic band in the world.",
-    "category": "Music Concert",
-    "place": "Room A",
-    "capacity": 138000,
-    "estimate": 138000,
-    "price": 150
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Conciertodemusica2.jpg",
-    "name": "Electronic Fest",
-    "date": "2021-01-22",
-    "description": "The best national and international DJs gathered in one place.",
-    "category": "Music Concert",
-    "place": "Room A",
-    "capacity": 138000,
-    "assistance": 110300,
-    "price": 250
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Maraton3.jpg",
-    "name": "10K for life",
-    "date": "2021-03-01",
-    "description": "Come and exercise, improve your health and lifestyle.",
-    "category": "Race",
-    "place": "Campo de FutbÃ³l",
-    "capacity": 30000,
-    "assistance": 25698,
-    "price": 3
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Maraton1.jpg",
-    "name": "15K NY",
-    "date": "2021-03-01",
-    "description": "We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-    "category": "Race",
-    "place": "New York",
-    "capacity": 3000000,
-    "assistance": 2569800,
-    "price": 3
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Libros7.jpg",
-    "name": "School's book fair",
-    "date": "2022-10-15",
-    "description": "Bring your unused school book and take the one you need.",
-    "category": "Book Exchange",
-    "place": "Room D1",
-    "capacity": 150000,
-    "estimate": 123286,
-    "price": 1
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Libros3.jpg",
-    "name": "Just for your kitchen",
-    "date": "2021-11-09",
-    "description": "If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-    "category": "Book Exchange",
-    "place": "Room D6",
-    "capacity": 130000,
-    "assistance": 90000,
-    "price": 100
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Cine3.jpg",
-    "name": "Batman",
-    "date": "2021-3-11",
-    "description": "Come see Batman fight crime in Gotham City.",
-    "category": "Cinema",
-    "place": "Room D1",
-    "capacity": 11000,
-    "assistance": 9300,
-    "price": 225
-    },
-    {
-    "image": "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
-    "name": "Avengers",
-    "date": "2022-10-15",
-    "description": "Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-    "category": "Cinema",
-    "place": "Room D1",
-    "capacity": 9000,
-    "estimate": 9000,
-    "price": 250
+
+//******************************************************************
+//*************************FIRST TABLE******************************
+//******************************************************************
+
+function funcionStats () {
+
+
+// obtener % de asistencia y capacidades
+const getPercentagesOfAttendanceAndCapacities = (requiredData) => {
+    const arrayWithCapacities = [];
+    const arrayWithAssistances = [];
+// console.log(events)
+console.log(requiredData)
+    events.forEach((event) => {
+        const assistance = event.assistance || event.estimate;
+        const eventPercentageOfAssistance = Math.round((assistance * 100) / event.capacity);
+        
+        arrayWithAssistances.push({
+            name: event.name,
+            percAssistance: eventPercentageOfAssistance
+        })
+
+        arrayWithCapacities.push({
+            name: event.name,
+            capacity: event.capacity
+        })
+    })
+
+    if (requiredData == "assistance") {
+        return arrayWithAssistances;
+    } else if (requiredData == "capacity") {
+        return arrayWithCapacities;
     }
-]
 }
 
-const eventsStats = objectEventsStats.eventos;
+// ordenar y cortar el array obtenido en la función anterior
+const orderValues = (arr, property, typeOrder) => {
+    const orderedArr = arr.sort((a, b) => {
+        return b[property] - a[property]
+    })
 
-// obtener array con todos los porcentajes de asistencia reales
-const allPercentagesAssistence = [];
+    if (typeOrder == "asc") {
+        orderedArr.reverse();
+    }
 
-const getHighestPercentagesOfAttendance = () => {
+    const number = get10PercentageOfEventsToBeRender(arr);
+
+    const arrayOfEventsToBeRender = orderedArr.slice(0, number);
+
+    return arrayOfEventsToBeRender;
+}
+
+// obtener el 10% del total de eventos
+const get10PercentageOfEventsToBeRender = (arr) => {
+    return Math.round(arr.length * 0.1);
+}
+
+// capturo los th de la tabla
+const firstTable = document.getElementById('first-table');
+const secondTable = document.getElementById('second-table');
+const thirdTable = document.getElementById('third-table');
     
-    for (let i=0; i < eventsStats.length; i++) {
-        if (eventsStats[i].hasOWnProperty('assistance')) {
-            const eventPercentageOfAttendance = Math.round((eventsStats[i].assistance * 100) / eventsStats[i].capacity);
-            console.log(eventPercentageOfAttendance)
-            allPercentagesAssistence.push(eventPercentageOfAttendance);
-            console.log(allPercentagesAssistence)
-        }
+// renderizo la data obtenida en la primera tabla
+const renderDataInFirstTable = () => {
+    const eventsWithHighestAssistances = orderValues(getPercentagesOfAttendanceAndCapacities("assistance"), "percAssistance", "desc");
+    
+    const eventsWithLowestAssistances = orderValues(getPercentagesOfAttendanceAndCapacities("assistance"), "percAssistance", "asc");
+    
+    const eventsWithLargerapacities = orderValues(getPercentagesOfAttendanceAndCapacities("capacity"), "capacity", "desc");
+
+    const numberOfRowsInTable = eventsWithHighestAssistances.length;
+    
+    for (let i=0; i < numberOfRowsInTable; i++) {
+        tableContent = `
+            <tr>
+                <td><span class="event-name">${eventsWithHighestAssistances[i].name}:</span> ${eventsWithHighestAssistances[i].percAssistance}%</td>
+                <td><span class="event-name">${eventsWithLowestAssistances[i].name}:</span> ${eventsWithLowestAssistances[i].percAssistance}%</td>
+                <td><span class="event-name">${eventsWithLargerapacities[i].name}:</span> ${eventsWithLargerapacities[i].capacity}</td>
+            </tr>
+        `
+
+        firstTable.insertAdjacentHTML("afterend", tableContent);
     }
 }
 
-// renderizar en tabla porcentajes
-getHighestPercentagesOfAttendance();
-    const highestPercentageAttendanceInHTML = document.getElementById('highest-attendance');
 
-const renderHighestPercentagesOfAttendance = () => {
+//******************************************************************
+//**********************SECOND & THIRD TABLE************************
+//******************************************************************
+
+// obtengo todas las categorías
+const getCategories = (events) => {
+    const categories = [];
+
+    events.map((event) => {
+        if (! categories.includes(event.category)) {
+            categories.push(event.category);
+        }
+    })
+    
+    return categories;
+}
 
 
+// creo array de objetos con la data que necesito para renderizar
+const getDataByCategory = (events) => {
+    const categories = getCategories(events);
+
+    const objectsWithDataToRender = [];
+
+    categories.map((category) => {
+        objectsWithDataToRender.push({
+            category: category,
+            revenues: getRevenuesByCategory(events, category),
+            percAssistance: getPercAssistanceByCategory(events, category)
+        });
+    })
+
+    return objectsWithDataToRender;
+}
 
 
+// calculo el array con el total de ganancias por categoría
+const getRevenuesByCategory = (events, category) => {
+    let totalRevenues = 0;
+
+    events.map((event) => {
+        if (event.category == category) {
+            const eventAssistance = event.assistance || event.estimate;
+
+            const eventRevenue = event.price * eventAssistance;
+
+            totalRevenues += eventRevenue;
+        }
+    })
+
+    return totalRevenues;
+}
+
+
+// calculo el array con el porcentaje de asistencia por categoría
+const getPercAssistanceByCategory = (events, category) => {
+    let totalCapacity = 0;
+    let totalAssistance = 0;
+
+    events.map((event) => {
+        if (event.category == category) {
+            totalCapacity += event.capacity;
+            totalAssistance += event.assistance || event.estimate;
+        }
+    })
+
+    const percAssistanceByCategory = Math.round((totalAssistance * 100) / totalCapacity);
+
+    return percAssistanceByCategory;
+}
+
+
+// renderizo la data obtenida en las últimas tablas
+const renderDataInLastTables = (events, table) => {
+
+    const numberOfRowsInTable = getCategories(events).length;
+
+    const dataByCategory = getDataByCategory(events);
+
+    for (let i=0; i < numberOfRowsInTable; i++) {
+        tableContent = `
+            <tr>
+                <td class="event-name">${dataByCategory[i].category}</td>
+                <td>$ ${dataByCategory[i].revenues}</td>
+                <td>${dataByCategory[i].percAssistance}%</td>
+            </tr>
+        `
+    }
+
+    table.insertAdjacentHTML("afterend", tableContent);
+}
+
+
+// ejecuto las funciones para renderizar en tabla
+renderDataInFirstTable();
+renderDataInLastTables(upcomingEvents, secondTable);
+renderDataInLastTables(pastEvents, thirdTable);
 }
